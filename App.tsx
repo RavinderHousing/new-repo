8d22e03b4ab8a38de192d0b3246203fb713063cb
Ravinder-agg <ravinder1@housing.com>
2024-03-06 11:32:04 +0530

    fsjnsfd

--- App.tsx
+++ App.tsx
@@ -96,7 +96,7 @@ function App(): JSX.Element {
               justifyContent: 'center',
               alignSelf: 'center',
               fontSize: 32
-            }} >new00022</Text>
+            }} >new000222</Text>
             <MagicText 
               width={200}
               height={200}
